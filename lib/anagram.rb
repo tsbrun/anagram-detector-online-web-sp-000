4a7f983dd3f7b97801a3579c2b90c2fa7da846e6
Anya Brun <tsbrun@icloud.com>
2020-08-11 18:22:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,5 +6,14 @@ class Anagram
   end
 
   def match(possible_matches)
+    # split @word into letters
+    word_letters = @word.split
+    # iterate over possible matches
+    # split each word into letters
+    # sort letters
+    # see if match
+    # if yes, add to "matches" array
+    # if no, move on to next word
+    # return matches array
   end
 end
